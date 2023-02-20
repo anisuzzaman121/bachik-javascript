62595672a49141902108dfe8000d0e4f2ca5abd5
anisuzzaman121 <anisuzzamna173@gmail.com>
2023-02-20 16:18:35 +0600

    Variable and Number

---
+++ Number/index.js
@@ -0,0 +1,39 @@
+var phn,gender,county;
+phn=01772875121;
+gender="mail ";
+county="Bangladesh ";
+document.write(gender);
+document.write(phn);
+document.write(county)
+
+var Name;
+var age;
+Name="anis";
+age=27;
+document.write(Name);
+document.write(age);
+Name="shakil";
+document.write(Name);
+
+//Number-----------------------
+var num=20;
+num=toString(num);
+console.log(typeof(num));
+
+var num="20";
+num=parseInt(num);
+console.log(typeof(num));
+
+var num ="20.5";
+num=parseFloat(num);
+console.log(typeof(num));
+
+var num=2.5678;
+console.log(num.toFixed(2));
+
+var num=2.46789;
+console.log(num.toPrecision(3));
+
+console.log(Number("12"))
+console.log(Number(true))
+console.log(Number(false))
