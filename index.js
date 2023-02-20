1856aa421148cc1547d2020050c193d662911fe8
anisuzzaman121 <anisuzzamna173@gmail.com>
2023-02-20 14:55:09 +0600

    Out put see

---
+++ index.js
@@ -0,0 +1,7 @@
+
+alert("Detelse")
+document.write("Name: Anisuzzaman </br>");
+document.write("Phone number: 01772875121 </br>");
+document.write("Gender: Mail </br>");
+document.write("Email: anisuzzaman173@gmail.com </br>");
+document.write("Age: 27")
